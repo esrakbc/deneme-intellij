d0fc558f9042de0cd376e77a5faf7ca5a73e9b8d
esrakbc <ekabaca09@gmail.com>
2023-02-26 15:57:57 +0300

    v4

--- src/main/java/Deneme.java
+++ src/main/java/Deneme.java
@@ -2,5 +2,7 @@ public class Deneme {
     public static void main(String[] args) {
         System.out.println("Merhaba");
         System.out.println("dünya");
+        System.out.println("B129");
+
     }
 }
